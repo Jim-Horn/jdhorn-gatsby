3b528097244a32fe26acbbe92704986cfd1f8bee
Jim Horn <jim@jdhorn.com>
2023-02-08 17:58:45 -0800

    Add default landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,34 @@
 import * as React from 'react';
-// import { Link } from 'gatsby';
-// import { StaticImage } from 'gatsby-plugin-image';
+import styled from 'styled-components';
+import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
 import Seo from '../components/seo';
 
+const CenteredContent = styled.main`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  font-family: monospace;
+  div {
+    margin: 1rem;
+    padding: 0.5rem;
+  }
+`;
+
 const HomePage = () => (
-  <Layout>
-    <h1>Hi</h1>
-  </Layout>
+  <CenteredContent>
+    <div>
+      <h1>Hi</h1>
+      <p>You've arrived at jdhorn.com</p>
+      <p>Actual content coming soon</p>
+      <p>
+        Existing stuff <Link to="/sitemap">here</Link>
+      </p>
+    </div>
+  </CenteredContent>
 );
 
 export const Head = () => <Seo title="Home" />;
