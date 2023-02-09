f3a3b278dbdf498110103bd9abb5405247fa339a
Jim Horn <jim@jdhorn.com>
2023-02-08 18:50:51 -0800

    Add markdown template

---
+++ src/pages/articles/{markdownRemark.frontmatter__slug}.jsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { graphql } from 'gatsby';
+import Layout from '../../components/layout';
+import styled from 'styled-components';
+
+const Tiny = styled.span`
+  font-size: small;
+`;
+
+export default function ArticleTemplate({ data }) {
+  const { markdownRemark } = data;
+  const { frontmatter, html } = markdownRemark;
+  return (
+    <Layout>
+      <div>
+        <h1>
+          {frontmatter.title}
+          <br />
+          <Tiny>{frontmatter.date}</Tiny>
+        </h1>
+        <div dangerouslySetInnerHTML={{ __html: html }} />
+      </div>
+    </Layout>
+  );
+}
+
+export const pageQuery = graphql`
+  query ($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        slug
+        title
+      }
+    }
+  }
+`;
