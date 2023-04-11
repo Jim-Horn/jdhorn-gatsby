dc02002eb6e1dd50d716149727e923fc14e6f739
Jim Horn <jim@jdhorn.com>
2023-04-11 06:16:52 -0700

    Pass more props to codepen

--- src/components/CodePen.tsx
+++ src/components/CodePen.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
 import Codepen from 'react-codepen-embed';
 
-const Code = ({ hash }: { hash: string }) => (
+const Code = ({
+  hash,
+  height = 300,
+  defaultTab = 'js',
+}: {
+  hash: string;
+  height: number;
+  defaultTab: string;
+}) => (
   <Codepen
     user="JDHorn"
-    defaultTab="js"
+    defaultTab={defaultTab}
     hash={hash}
+    height={height}
     // @ts-ignore
     loader={() => <div>Loading...</div>}
   />
