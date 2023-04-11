c2452140c0bf5400cd794eb245eeb4338219529b
Jim Horn <jim@jdhorn.com>
2023-04-11 06:22:11 -0700

    Make pen editable

--- src/components/CodePen.tsx
+++ src/components/CodePen.tsx
@@ -5,16 +5,19 @@ const Code = ({
   hash,
   height = 300,
   defaultTab = 'js',
+  editable = false,
 }: {
   hash: string;
   height: number;
   defaultTab: string;
+  editable: boolean;
 }) => (
   <Codepen
     user="JDHorn"
     defaultTab={defaultTab}
     hash={hash}
     height={height}
+    editable={editable}
     // @ts-ignore
     loader={() => <div>Loading...</div>}
   />
