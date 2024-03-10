56f59c1643ca4d2ae125e25468b10f9609e17b47
Jim Horn <jim@jdhorn.com>
2024-03-10 15:41:08 -0700

    Revert "Refactor component exports for simplicity"
    This reverts commit 9e0dcfd5a9d0c86cfd5beedc1504dc0a7bf506a0.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,25 @@
-export * from './CodePen';
-export * from './ExternalLink';
-export * from './Header';
-export * from './ImageHeader';
-export * from './Layout';
-export * from './ListTags';
-export * from './Posts';
-export * from './Seo';
-export * from './Sitemap';
-export * from './Tags';
-export * from './PassWords';
+import { ExternalLink } from './ExternalLink';
+import { Header } from './Header';
+import { ImageHeader } from './ImageHeader';
+import { Layout } from './Layout';
+import { ListTags } from './ListTags';
+import { Pen } from './CodePen';
+import { Posts } from './Posts';
+import { Seo } from './Seo';
+import { Sitemap } from './Sitemap';
+import { Tags } from './Tags';
+import { PassWords } from './PassWords';
+
+export {
+  ExternalLink,
+  Header,
+  ImageHeader,
+  Layout,
+  ListTags,
+  Pen,
+  Posts,
+  Seo,
+  Sitemap,
+  Tags,
+  PassWords,
+};
